d1225620a3782312dc56235901be34214884674b
Pablo Salazar <pablo.s.1840@gmail.com>
2021-06-19 22:06:52 -0500

    Create class "PostulanteController" and "PostulanteResource"

---
+++ src/test/java/com/rommel/pablo/Api/resources/PostulanteResourceTest.java
@@ -0,0 +1,34 @@
+package com.rommel.pablo.Api.resources;
+
+
+import com.rommel.pablo.Api.entities.Postulante;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostulanteResourceTest {
+
+    @Autowired
+    private RestService restService;
+    private Postulante postulante;
+
+    @Test
+    public void getAllPostulante() {
+        String json = restService
+                .restBuilder(new RestBuilder<String>().clazz(String.class))
+                .path(PostulanteResource.POSTULANTE).get().build();
+        System.out.println(json);
+    }
+
+    @Test
+    public void getById() {
+        String json = restService
+                .restBuilder(new RestBuilder<String>().clazz(String.class))
+                .path(PostulanteResource.POSTULANTE).path(PostulanteResource.ID).expand(201).get().build();
+        System.out.println(json);
+    }
+}
